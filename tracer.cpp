302d7b3f5e3fb71711cbb36ecb7296618df71ad8
Roman Zenka <zenka.roman@mayo.edu>
2013-05-13 12:25:00 -0500

    Tracer interface simplified

--- tracer.cpp
+++ tracer.cpp
@@ -2,135 +2,50 @@
 
 #ifdef TRACER
 
-void tracer_hdr(const char * op) {
-	cout << "[TRACER]\t" << op << "\t";
-}
-
-void tracer_ftr() {
-	cout << "\n";
-}
-
-void tracer_hdr(const char * op, const void * obj) {
-	tracer_hdr(op);
-	cout << obj << "\t";
-}
-
-void tracer_label(const void *x, const char* name) {
-	tracer_hdr("label", x);
-	cout << name;
-	tracer_ftr();
-}
-
-void tracer_var(const void *x, const char* name, const char *file, int linenum) {
-	tracer_hdr("var", x);
-	cout << name << "\t" << file << "\t" << linenum;
-	tracer_ftr();
-}
-
-void tracer_unlink(const void *x) {
-	tracer_hdr("unlink", x);
-	tracer_ftr();
-}
-
-void tracer_start(const void *x) {
-	tracer_hdr("start", x);
-	tracer_ftr();
-}
-
-void tracer_dump_hdr(const void *object, const char* type) {
-	tracer_hdr("dump", object);
-	cout << type << "\t";
-}
+#include "myrimatchSpectrum.h"
 
 void tracer_dump(const string& x) {
-	tracer_dump_hdr(&x, "std::string");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "std::string" << '\t' << x;
 }
 
 void tracer_dump(const vector<string>& x) {
-	tracer_dump_hdr(&x, "std::vector<std::string>");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "std::vector<string>" << '\t' << x;
+}
+
+void tracer_dump(const map<string, string>& x) {
+	cout << &x << '\t' << "std::map<string, string>" << '\t' << x;
+}
+
+void tracer_dump(const vector<double>& x) {
+	cout << &x << '\t' << "std::vector<double>" << '\t' << x;
 }
 
 void tracer_dump(const int &x) {
-	tracer_dump_hdr(&x, "int");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "int" << '\t' << x;
 }
 
 void tracer_dump(const float &x) {
-	tracer_dump_hdr(&x, "float");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "float" << '\t' << x;
 }
 
 void tracer_dump(const double &x) {
-	tracer_dump_hdr(&x, "double");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "double" << '\t' << x;
 }
 
 void tracer_dump(const char &x) {
-	tracer_dump_hdr(&x, "char");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "char" << '\t' << x;
 }
 
 void tracer_dump(const IntegerSet &x) {
-	tracer_dump_hdr(&x, "IntegerSet");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "IntegerSet" << '\t' << x;
 }
 
 void tracer_dump(const boost::regex &x) {
-	tracer_dump_hdr(&x, "boost::regex");
-	cout << x;
-	tracer_ftr();
+	cout << &x << '\t' << "boost::regex" << '\t' << x;
 }
 
-void tracer_dump(const void *x, const char *type, const char * representation) {
-	tracer_dump_hdr(x, type);
-	cout << representation;
-	tracer_ftr();
+void tracer_dump(const freicore::myrimatch::SpectraList &x) {
+	cout << &x << '\t' << "SpectraList" << '\t' << "List of " << x.size() << " spectra";
 }
-
-void tracer_dump(const void *x, const char * type, const string &representation) {
-	tracer_dump_hdr(x, type);
-	cout << representation;
-	tracer_ftr();
-}
-
-void tracer_reason(const void *x, const char * reason) {
-	tracer_hdr("reason", x);
-	cout << reason;
-	tracer_ftr();
-}
-
-void tracer_op_start(const char *name, const char *file, int linenum) {
-	tracer_hdr("op_start");
-	cout << name << "\t" << file << "\t" << linenum;
-	tracer_ftr();
-}
-
-void tracer_op_input(const void *x) {
-	tracer_hdr("op_in");
-	cout << x;
-	tracer_ftr();
-}
-
-void tracer_op_output(const void *x) {
-	tracer_hdr("op_out");
-	cout << x;
-	tracer_ftr();
-}
-
-void tracer_op_end(const char *name, const char *file, int linenum) {
-	tracer_hdr("op_end");
-	cout << name << "\t" << file << "\t" << linenum;
-	tracer_ftr();
-}
-
 
 #endif // TRACER
--- tracer.hpp
+++ tracer.hpp
@@ -3,76 +3,57 @@
 
 #include "stdafx.h"
 
-#define TRACER 1
+#define TRACER_ENABLED 1
+#define HEAP 1
+#define STACK 2
 
-#ifdef TRACER
+#define READ 1
+#define WRITE 2
+#define NOTE 3
 
-void tracer_label(const void *x, const char* name);
-void tracer_var(const void *x, const char* name, const char *file, int linenum);
-void tracer_unlink(const void *x);
-void tracer_start(const void *x);
+#ifdef TRACER_ENABLED
 
 void tracer_dump(const string& x);
 void tracer_dump(const vector<string>& x);
+void tracer_dump(const map<string, string>& x);
+void tracer_dump(const vector<double>& x);
 void tracer_dump(const int &x);
 void tracer_dump(const float &x);
 void tracer_dump(const double &x);
 void tracer_dump(const char &x);
 void tracer_dump(const IntegerSet &x);
 void tracer_dump(const boost::regex &x);
-void tracer_dump(const void *x, const char * type, const char * representation);
-void tracer_dump(const void *x, const char * type, const string &representation);
 
-void tracer_reason(const void *x, const char * reason);
+namespace freicore {
+namespace myrimatch {
+struct SpectraList;
+}
+}
+void tracer_dump(const freicore::myrimatch::SpectraList &x);
 
-void tracer_op_start(const char *name, const char *file, int linenum);
-void tracer_op_input(const void *x);
-void tracer_op_output(const void *x);
-void tracer_op_end(const char *name, const char *file, int linenum);
-
-// Give a name to a memory address
-#define TRACER_LABEL(x, name) tracer_label(&(x), name)
-// Unlink the label before the memory address ceases to exist
-#define TRACER_UNLINK(x) tracer_unlink(&(x))
-// Mark end of a 'logical lifetime' for a particular object
-#define TRACER_START(x) tracer_start(&(x))
-
-// Dump contents of a particular object
-#define TRACER_DUMP(x) tracer_dump(x)
-
-// Dump contents of a particular object with a specified string representation
-#define TRACER_DUMP_R(x, type, representation) tracer_dump(&(x), type, representation)
-
-// Why has the object value changed here?
-#define TRACER_REASON(x, reason) tracer_reason(&(x), reason);
+#define TRACER(variable, operation, heap, note) { cout << "[TRACER]" << '\t' << "dump"; tracer_dump(variable); cout << '\t' << heap << '\t' << operation << '\t' << note << '\t' << __FILE__ << '\t' << __LINE__ << '\n'; }
+#define TRACER_P(variable, type, representation, operation, heap, note) { cout << "[TRACER]" << '\t' << "dump" << &(variable) << '\t' << type << '\t' << representation <<  '\t' << heap << '\t' << operation << '\t' << note << '\t' << __FILE__ << '\t' << __LINE__ << '\n'; }
+// Reference a variable in an operation, do not dump all its info
+#define TRACER_REF(variable, operation, heap, note) { cout << "[TRACER]" << '\t' << "ref" << &(variable) << heap << '\t' << operation << '\t' << note << '\t' << __FILE__ << '\t' << __LINE__ << '\n'; }
+#define TRACER_UNLINK(variable) { cout << "[TRACER]" << '\t' << "unlink" << '\t' << &(variable) << '\t' __FILE__ << '\t' << __LINE__ << '\n'; }
 
 // Start operation of a given name
-#define TRACER_OP_START(name) { tracer_op_start(name, __FILE__, __LINE__); }
-// The operation uses a particular input
-#define TRACER_OP_INPUT(x) { tracer_var(&(x), #x, __FILE__, __LINE__); tracer_op_input(&(x)); }
-#define TRACER_OP_IN_DUMP(x) { TRACER_OP_INPUT(x); TRACER_DUMP(x); }
-#define TRACER_OP_IN_DUMP_R(x, type, representation) { TRACER_OP_INPUT(x); TRACER_DUMP_R(x, type, representation); }
-// The operation produces a particular output
-#define TRACER_OP_OUTPUT(x) { tracer_var(&(x), #x, __FILE__, __LINE__); tracer_op_output(&(x)); }
-#define TRACER_OP_OUT_DUMP(x) { TRACER_OP_OUTPUT(x); TRACER_DUMP(x); }
-#define TRACER_OP_OUT_DUMP_R(x, type, representation) { TRACER_OP_OUTPUT(x); TRACER_DUMP_R(x, type, representation); }
+#define TRACER_OP_START(name) { cout << "[TRACER]" << '\t' << "op_start" << '\t' << name << '\t' << __FILE__ << '\t' << __LINE__ << '\n'; }
 // End operation of a given name
-#define TRACER_OP_END(name) tracer_op_end(name, __FILE__, __LINE__)
+#define TRACER_OP_END(name) { cout << "[TRACER]" << '\t' << "op_end" << '\t' << name << '\t' << __FILE__ << '\t' << __LINE__ << '\n'; }
+#define TRACER_S2S(variable) lexical_cast<string>(variable)
 
-#else
+#else // !TRACER_ENABLED
 
-#define TRACER_LABEL(x, name)
-#define TRACER_UNLINK(x)
-#define TRACER_DUMP(x)
-#define TRACER_DUMP_R(x, type, representation)
-#define TRACER_REASON(x, reason)
+#define TRACER(variable, operation, heap, note)
+#define TRACER_R(variable, type, representation, operation, heap, note)
+#define TRACER_UNLINK(variable)
 #define TRACER_OP_START(name)
-#define TRACER_OP_INPUT(x)
-#define TRACER_OP_OUTPUT(x)
-#define TRACER_OP_OUT_DUMP(x, name)
-#define TRACER_OP_OUT_DUMP_R(x, name, type, representation)
 #define TRACER_OP_END(name)
-#endif
+#define TRACER_S2S(variable)
+
+#endif // TRACER_ENABLED
 
 
 #endif 
+
